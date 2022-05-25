49a591928ce4288bb0d187a260c0ba75dd2f9930
Jan Premzl <jan.premzl16@gmail.com>
2022-05-25 21:34:31 +0200

    Adding components to canvas

--- src/app/components/picker/basic/basic.component.ts
+++ src/app/components/picker/basic/basic.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
-  styleUrls: ['./basic.component.scss']
+  styleUrls: ['./basic.component.scss'],
 })
-export class BasicComponent implements OnInit {
+export class BasicComponent implements OnInit, AfterViewInit {
+  @ViewChild('canvas', { static: false })
+  private canvas!: ElementRef<HTMLCanvasElement>;
 
-  constructor() { }
+  private fabricCanvas!: fabric.Canvas;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.fabricCanvas = new fabric.Canvas('canvas', {
+      height: this.canvas.nativeElement.offsetHeight,
+      width: this.canvas.nativeElement.offsetWidth,
+    });
+
+    this.fabricCanvas.on('mouse:down', (e) => {
+      this.addTriangle(e.absolutePointer!.x!, e.absolutePointer!.y!);
+    });
   }
 
+  ngOnInit(): void {}
+
+  public addTriangle(x: number, y: number): void {
+    this.fabricCanvas.add(
+      new fabric.Rect({
+        top: y - 50,
+        left: x - 50,
+        width: 100,
+        height: 100,
+        fill: 'red',
+      })
+    );
+  }
+
+  public clear(): void {
+    this.fabricCanvas.clear();
+  }
 }
--- src/app/components/picker/basic/basic.module.ts
+++ src/app/components/picker/basic/basic.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BasicRoutingModule } from './basic-routing.module';
 import { BasicComponent } from './basic.component';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    BasicComponent
-  ],
-  imports: [
-    CommonModule,
-    BasicRoutingModule
-  ]
+  declarations: [BasicComponent],
+  imports: [CommonModule, BasicRoutingModule, MatButtonModule],
 })
-export class BasicModule { }
+export class BasicModule {}
